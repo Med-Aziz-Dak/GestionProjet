6b8d034e5c6f1157f82d1037a05f5a5b910fc71f
RajaBelHadjRomdhane <belhadjraja17@gmail.com>
2025-03-30 01:27:03 +0100

    création du sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Sidebar from './sidebar'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+   <div className ='App' >
+    <header>Your Projects</header>
+    <Sidebar ></Sidebar>
+   </div>
   )
 }
 
---
+++ src/sidebar.jsx
@@ -0,0 +1,18 @@
+const Sidebar = ( ) => {
+    return (
+        <div className ="sidebar">
+            <ul>
+                <li>
+                    <a href="/">Add a Project</a>
+                </li>
+
+                <li>
+                    <a href="/">SigIn</a></li>
+                <li><a href="/">SigOut</a></li>
+
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar
